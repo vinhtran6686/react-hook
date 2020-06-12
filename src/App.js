a1a04b87cf84e7f0b39437472f2ceb1f6e991329
vinhtv7 <vinhtv7@fpt.com.vn>
2020-06-12 16:37:07 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
-
 function App() {
-  return (
+  const name = useRef();
+  const age = useRef();
+  const sex = useRef();
+  const submit = useRef();
+  useEffect(() => {
+    name.current.focus();
+  }, [])
+  function keyDownPressHandle(e) {
+    if (e.keyCode === 13) {
+      if (e.target.id === 'nameInput') {
+        age.current.focus();
+      } if (e.target.id === 'ageInput') {
+        sex.current.focus();
+      } if (e.target.id === 'sexInput') {
+        submit.current.focus();
+      }
+    }
+  }
+  function onClickHandle() {
+    alert('submittt');
+  } return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='row'>
+          <span>Name</span> <input ref={name} id="nameInput" type="text" onKeyDown={keyDownPressHandle}></input>
+        </div>
+        <div className='row'>
+          <span>age</span> <input ref={age} id="ageInput" type="number" onKeyDown={keyDownPressHandle}></input>
+        </div>
+        <div className='row'>
+          <span>Sex</span> <input ref={sex} id="sexInput" type="checkbox" onKeyDown={keyDownPressHandle}></input>
+        </div>
+        <div className='row'>
+          <button ref={submit} onClick={onClickHandle} id="submitButton" type="submit" onKeyDown={keyDownPressHandle}>Submit</button>
+        </div>
       </header>
-    </div>
-  );
+    </div>);
 }
-
 export default App;
