85eac5ce8e22697ec57152f2d55366a3e4805ff2
vinhtv7 <vinhtv7@fpt.com.vn>
2020-06-30 18:14:10 +0700

    Update promise

--- src/useEffect/example4.js
+++ src/useEffect/example4.js
@@ -2,33 +2,33 @@
 import React, { useEffect } from 'react';
 function App() {
      useEffect(() => {
-          function printNumber(number, callback) {
-               setTimeout(
-                    () => {
-                         callback()
-                    },
-                    Math.floor(Math.random() * 100) + 1
-               )
-          }
-     
-          function printAll() {
-               printNumber(1, function () {
-                    console.log('this 1')
-                    printNumber(2, function () {
-                         console.log('this 2')
-                         printNumber(3, function () {
-                              console.log('this 3')
-                              printNumber(4, function () {
-                                   console.log('this 4')
-                              })
-                         })
-                    })
+          function printNumber(number) {
+               return new Promise((resolve, reject) => {
+                    setTimeout(
+                         () => {
+                              if (number === 0) {
+                                   reject();
+                              } else {
+                                   console.log('this is number:' + number);
+                                   resolve();
+                              }
+                         },
+                         1000
+                    )
                })
           }
-          printAll();
-          // cách này dẫn đến tình trạng khi lồng vào nhau quá nhiều sẽ bị callback hell 
+          printNumber(1)
+               .then(() => printNumber(2))
+               .then(() => printNumber(3))
+               .then(() => printNumber(4))
+               .then(() => printNumber(5))
+               .then(() => printNumber(6))
+               .then(() => printNumber(7))
+               .then(() => printNumber(8))
+               .reject(() => console.log("number &lt; 0"))
+          // theo cách này vẫn phải truyền vào 1 hàm mà hàm đó lại gọi đến promise khác => promise hell 
      }, [])
-    
+
      return (
           <div className="App">
                <header className="App-header">
