63843eb695af5d87e57fcc4b5747a9476187cd95
vinhtran6686 <37211782+vinhtran6686@users.noreply.github.com>
2020-07-03 11:23:57 +0700

    Add custom fetch API with hook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './usePrevious/App';
+import App from './useCustomFetch/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
---
+++ src/useCustomFetch/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import '../App.css';
+import useCustomFetch from './hooks/useCustomFetch';
+
+
+function App() {
+  const [url, setUrl] = useState(null);
+  const [data, loading, error] = useCustomFetch(url);
+
+  function getFollowers(e){
+    if(e.key === 'Enter'){
+      setUrl("https://api.github.com/users/" + e.target.value);
+    }
+  }
+  console.log(data);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          GitID:
+          <input onKeyPress={getFollowers}></input>
+
+          {loading && url  && <div>Loading ....</div>}
+          {!loading && data && data.rData && data.rData.followers && (
+            <div>Followers: {data.rData.followers}</div>
+          )}
+          {error && <div>Error: {error}</div>}
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
